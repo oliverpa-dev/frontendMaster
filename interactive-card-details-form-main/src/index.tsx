2fee61e1e32d06afc538374491c77b8a70cd4fb8
Oliver <oliver.paszkiewicz0@gmail.com>
2024-04-29 13:05:20 +0200

    Update: import card component

--- interactive-card-details-form-main/src/index.tsx
+++ interactive-card-details-form-main/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Card from "./componenets/card";
+import Card from "./components/card";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
