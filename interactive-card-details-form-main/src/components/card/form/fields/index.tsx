b40b06da0aed34f1e8efe1cf1e85f131794575c9
Oliver <oliver.paszkiewicz0@gmail.com>
2024-04-29 13:06:16 +0200

    Add: layout for the fields

---
+++ interactive-card-details-form-main/src/components/card/form/fields/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Field = () => {
+  return (
+    <div className="card-fields">
+      <div className="card-fields__field">
+        <div className="card-fields__field-label">
+          <label>cardholder name</label>
+        </div>
+        <div className="card-fields__field-input">
+          <input type="string" />
+        </div>
+        <div className="card-fields__field-error">
+          <span>Error</span>
+        </div>
+      </div>
+      <div className="card-fields__field">
+        <div className="card-fields__field-label">
+          <label>card number</label>
+        </div>
+        <div className="card-fields__field-input">
+          <input type="string" />
+        </div>
+        <div className="card-fields__field-error">
+          <span>Error</span>
+        </div>
+      </div>
+      <div className="card-fields__button">
+        <button>confirm</button>
+      </div>
+    </div>
+  );
+};
