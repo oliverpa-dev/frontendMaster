10e796524084098c1a15eece8814916425106436
Oliver <oliver.paszkiewicz0@gmail.com>
2024-04-29 13:04:33 +0200

    Add: card component

---
+++ interactive-card-details-form-main/src/components/card/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Form } from "./form";
+import "./main.scss";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <Form />
+    </div>
+  );
+};
+
+export default Card;
