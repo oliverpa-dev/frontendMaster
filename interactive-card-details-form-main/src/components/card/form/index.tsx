0116c68ab400aaf5cfa912911b51cd90fc39755f
Oliver <oliver.paszkiewicz0@gmail.com>
2024-05-02 11:32:45 +0200

    Add: comments for the function

--- interactive-card-details-form-main/src/components/card/form/index.tsx
+++ interactive-card-details-form-main/src/components/card/form/index.tsx
@@ -9,11 +9,16 @@ import "./main.scss";
 - Reset the form when the user clicks "Continue" on the completed state
  */
 
-// Check the value type
-const checkType = (value: string, cb: () => void) => {
+/**
+ * Function that checks the type of the passed in value
+ * @param value - string or number
+ * @param cb - callback functions that sets the boolean value to true of false
+ */
+const checkType = (value: string, booleanCheck: () => void) => {
   if (typeof value === "string") {
-    // Change the boolean value
-    cb();
+    booleanCheck();
+  } else {
+    booleanCheck();
   }
 };
 
